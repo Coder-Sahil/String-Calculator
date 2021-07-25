8e6cc2fafb7d0b03ce762c95de049fd7ad02ca29
Sahil Trivedi <1SahilTrivedi@gmail.com>
2021-07-25 20:34:49 +0530

    Initial Commit - Project Structure

---
+++ src/main/java/com/sahiltrivedi/calculator/StringCalculator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sahiltrivedi.calculator;
+
+/**
+ *
+ * @author Trivedi
+ */
+public class StringCalculator {     
+    
+    public int Add(String numbers)
+    {        
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+       
+    }
+}
---
+++ src/test/java/com/sahiltrivedi/calculator/StringCalculatorTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sahiltrivedi.calculator;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Trivedi
+ */
+public class StringCalculatorTest {
+    
+    public StringCalculatorTest() {
+    }
+    
+    private static StringCalculator strCal;
+    
+    @BeforeAll
+    public static void setUpClass() {
+        strCal = new StringCalculator();
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of Add method, of class StringCalculator.
+     */
+    
+    
+    
+    
+   
+    
+}
