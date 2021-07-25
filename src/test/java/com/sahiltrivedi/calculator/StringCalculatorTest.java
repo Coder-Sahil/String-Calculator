c1896919aa528d7b7958c157b84dece12e28e60b
Sahil Trivedi <1SahilTrivedi@gmail.com>
2021-07-26 00:01:06 +0530

    Revert "Implement Test Case For Invalid Single Input - Failed"
    This reverts commit 3c74ca3098fca0cdfa7a4b43f7fb5abd308b8dd2.

--- src/test/java/com/sahiltrivedi/calculator/StringCalculatorTest.java
+++ src/test/java/com/sahiltrivedi/calculator/StringCalculatorTest.java
@@ -68,22 +68,7 @@ public class StringCalculatorTest {
         int result = strCal.Add(numbers);
         assertEquals(expResult, result);      
     }
-        
-    @Test
-    public void invalidSingleStringThrowException(){
-                
-        String numbers = "XYZ";
-        Exception result = null;
-        try
-        {
-            int res = strCal.Add(numbers);
-        }
-        catch(Exception ex)
-        {
-            result = ex;
-        }
-        assertEquals(IllegalArgumentException.class, result);
-    }
+    
     
    
     
