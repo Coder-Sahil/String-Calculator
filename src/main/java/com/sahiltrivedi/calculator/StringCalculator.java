c7b8db9b223f683caa56d165ad4e6993c083beeb
Sahil Trivedi <1SahilTrivedi@gmail.com>
2021-07-25 22:21:01 +0530

    Test Case For Empty String - PASSED

--- src/main/java/com/sahiltrivedi/calculator/StringCalculator.java
+++ src/main/java/com/sahiltrivedi/calculator/StringCalculator.java
@@ -12,7 +12,10 @@ package com.sahiltrivedi.calculator;
 public class StringCalculator {     
     
     public int Add(String numbers)
-    {        
+    {
+        if(numbers == "")
+            return 0;
+        
         return -1;
     }
     
