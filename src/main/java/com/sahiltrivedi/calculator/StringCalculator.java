ad0ab8891e2c2094c49a8f3b87723965974fc1ac
Sahil Trivedi <1SahilTrivedi@gmail.com>
2021-07-26 01:16:39 +0530

    Test Case For delimeter - Passed 2

--- src/main/java/com/sahiltrivedi/calculator/StringCalculator.java
+++ src/main/java/com/sahiltrivedi/calculator/StringCalculator.java
@@ -5,6 +5,8 @@
  */
 package com.sahiltrivedi.calculator;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Trivedi
@@ -23,16 +25,37 @@ public class StringCalculator {
             {
                 sum = Integer.parseInt(numberArray[0]);
             }
+            else if(Pattern.matches("[0-9]", numberArray[0]) == false)
+            {
+                String delimeter = numberArray[0].substring(2);
+                String[] tempNumberArray = numbers.split("\n",2);
+                numberArray = tempNumberArray[1].split(delimeter);
+                sum = totalSum(numberArray);
+            }
             else
             {
+                sum = totalSum(numberArray);
+                /*
                 for(String numString : numberArray )
                 {
                     int num = Integer.parseInt(numString);
                     sum = sum + num;
                 }
+                */
             }
         }
         
+        return sum;
+    }
+    
+    private int totalSum(String[] numberArray)
+    {
+        int sum = 0;
+        for(String numString : numberArray )
+            {
+                int num = Integer.parseInt(numString);
+                sum = sum + num;
+            }
         return sum;
     }
     
