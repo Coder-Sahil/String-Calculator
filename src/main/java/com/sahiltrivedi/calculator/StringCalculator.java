4e38332e2ebf33598a89579067f6f987873e8fe9
Sahil Trivedi <1SahilTrivedi@gmail.com>
2021-07-26 16:22:41 +0530

    Test Case For Negative Numbers - Passed

--- src/main/java/com/sahiltrivedi/calculator/StringCalculator.java
+++ src/main/java/com/sahiltrivedi/calculator/StringCalculator.java
@@ -13,51 +13,79 @@ import java.util.regex.Pattern;
  */
 public class StringCalculator {     
     
-    public int Add(String numbers)
+    public int Add(String numbers) throws Exception
     {
         int sum = 0;
-        if(numbers == "" || numbers == null)
-            return 0;
-        else
+        try
         {
-            String[] numberArray = numbers.split("[, \n]");
-            if(numberArray.length == 1)
-            {
-                sum = Integer.parseInt(numberArray[0]);
-            }
-            else if(Pattern.matches("[0-9]", numberArray[0]) == false)
-            {
-                String customDelimeter = numberArray[0].substring(2);
-                String delimeter = "[" + customDelimeter + "\n" +"]";
-                String[] tempNumberArray = numbers.split("\n",2);
-                numberArray = tempNumberArray[1].split(delimeter);
-                sum = totalSum(numberArray);
-            }
+            if(numbers == "" || numbers == null)
+                return 0;
             else
             {
-                sum = totalSum(numberArray);
-                /*
-                for(String numString : numberArray )
+                String[] numberArray = numbers.split("[, \n]");
+                if(numberArray.length == 1)
                 {
-                    int num = Integer.parseInt(numString);
-                    sum = sum + num;
+                    sum = Integer.parseInt(numberArray[0]);
                 }
-                */
+                else if(Pattern.matches("[0-9]", numberArray[0]) == false)
+                {
+                    String customDelimeter = numberArray[0].substring(2);
+                    String delimeter = "[" + customDelimeter + "\n" +"]";
+                    String[] tempNumberArray = numbers.split("\n",2);
+                    numberArray = tempNumberArray[1].split(delimeter);
+                    sum = totalSum(numberArray);
+                }
+                else
+                {
+                    sum = totalSum(numberArray);
+                    /*
+                    for(String numString : numberArray )
+                    {
+                        int num = Integer.parseInt(numString);
+                        sum = sum + num;
+                    }
+                    */
+                }
             }
         }
+        catch(Exception exp)
+        {
+            throw exp;
+        }
+        return sum;
+    }
+    
+    private int totalSum(String[] numberArray) throws Exception
+    {
+        int sum = 0;
+        String negativeNumbers = "";
+        boolean negativeFound = false; 
+        for(String numString : numberArray )
+            {
+                int num = Integer.parseInt(numString);
+                if(checkNegative(num))
+                {
+                    negativeFound = true;
+                    negativeNumbers += numString + ",";
+                }
+                else
+                {
+                    sum = sum + num;
+                }                
+            }
+        if(negativeFound)
+                    throw new Exception("negative not allowed " + negativeNumbers.substring(0, negativeNumbers.length() - 1));
         
         return sum;
     }
     
-    private int totalSum(String[] numberArray)
+    private boolean checkNegative(int number)
     {
-        int sum = 0;
-        for(String numString : numberArray )
-            {
-                int num = Integer.parseInt(numString);
-                sum = sum + num;
-            }
-        return sum;
+        boolean isNegative = false;
+        if(number < 0)
+            return isNegative = true;
+        else
+            return isNegative;
     }
     
     public static void main(String[] args) {
